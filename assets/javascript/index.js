b14050f337996b0b59e20c61c97ddb3514179cba
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2019-10-05 20:32:16 -0700

    something is working correctly. adding now before something breaks...

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,28 +1,49 @@
-// Create Arrays for Computer Choices - alphabet
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// variables
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Variables for functions
-var wins = 0;
-var loss = 0;
-var guessRemain = 9;
-var userGuess = [];
+var wins = 0;
+var losses = 0;
+var guessRemain = 9; //total guesses per round
+var userGuessSoFar = []; //user choice array
 
-// create a function to run whenever user presses a key
-document.onkeyup = function (event) {
+var userGuessChoices = document.getElementById("userguess");
+var winPoints = document.getElementById("wins");
+var guessRemaining = document.getElementById("guess");
+var lossPoints = document.getElementById("loss");
+var guessSoFar = document.getElementById("userguess");
 
-    // User key pressed, triggers event
-    var userGuess = event.key;
-
-    // Randomly chooses a choice from the options in array
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    // Logic to determine the  outcome of game (win/loss/tie)
-    if ((userGuess === computerChoices)) {
-
-    }
-}
-// Computer is able to select a random letter from the array
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+function updateDisplays() {
+    winPoints.textContent = wins;
+    guessRemaining.textContent = guessRemain;
+    lossPoints.textContent = losses;
+}
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    userGuessSoFar.push(userGuess);
+    userGuessChoices.textContent = userGuessSoFar;
 
-// jQuery button animations...extra extra
+    if (userGuess == computerGuess) {
+        wins++;
+        guessRemain = 9;
+        userGuessSoFar = [];
+        computerGuess = computerChoices[Math.floor(Math.random() * letters.length)];
+    }
+
+    else {
+        guessRemain--;
+
+        if (guessRemain == 0) {
+            losses++;
+            userGuessSoFar = [];
+            guessRemain = 9;
+        }
+    }
+
+    updateDisplays();
+}
+updateDisplays();
+
+console.log(computerGuess);
+console.log(userGuessSoFar);
